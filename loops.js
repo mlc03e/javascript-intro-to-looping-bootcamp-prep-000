a5dc36daea35884d88a07a338f2803762f512dc0
Meghan Campbell <mlc03e@gmail.com>
2018-09-15 18:12:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array){
 for (let i = 0; i < 25; i++);
 if (i === 1) console.log("I am ${i} strange loop.")
 else console.log("I am ${i} strange loops.")
-
+}
 
 function whileLoop(n) {
 let countdown = n;
